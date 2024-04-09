4fcd152d749f6676ae9596cd173404445d822db5
devpark435 <devpark435@gmail.com>
2024-04-09 21:48:28 +0900

    feat #1 - StackView add label & constraint

--- GithubProfile/Presentation/Main/ViewController/MainViewController.swift
+++ GithubProfile/Presentation/Main/ViewController/MainViewController.swift
@@ -6,15 +6,60 @@
 //
 
 import UIKit
+import Then
+import Kingfisher
+
 
 class MainViewController: UIViewController {
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var stackView: UIStackView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        GetUserData.shared.getUserData()
+        GetUserData.shared.getUserData { [weak self] result in
+            switch result {
+            case .success:
+                DispatchQueue.main.async {
+                    self?.updateProfile()
+                }
+            case .failure(let error):
+                print("Error: \(error)")
+            }
+        }
     }
 
-
+    func updateProfile() {
+        if let avatarURL = URL(string: User.shared.avatarURL) {
+            profileImageView.kf.setImage(with: avatarURL)
+        }
+        profileImageView.layer.cornerRadius = profileImageView.frame.width / 2
+        let nameLabel = UILabel().then {
+            $0.text = "\(User.shared.name ?? "")"
+            $0.font = UIFont.systemFont(ofSize: 17)
+            $0.textColor = .black
+        }
+        
+        let emailLabel = UILabel().then {
+            $0.text = "\(User.shared.login)"
+            $0.font = UIFont.systemFont(ofSize: 13)
+            $0.textColor = .black
+        }
+        
+        let locationLabel = UILabel().then {
+            $0.text = "\(User.shared.createdAt)"
+            $0.font = UIFont.systemFont(ofSize: 13)
+            $0.textColor = .black
+        }
+        
+        stackView.addArrangedSubview(nameLabel)
+        stackView.addArrangedSubview(emailLabel)
+        stackView.addArrangedSubview(locationLabel)
+        
+        stackView.axis = .vertical
+        stackView.alignment = .leading
+        stackView.spacing = 8
+    }
 }
 
