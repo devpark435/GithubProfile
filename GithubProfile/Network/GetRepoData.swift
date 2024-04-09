c482caa043e0edc1c001e72ef88cf6b4e6777f4b
devpark435 <devpark435@gmail.com>
2024-04-10 02:43:13 +0900

    Edit - Lint

--- GithubProfile/Network/GetRepoData.swift
+++ GithubProfile/Network/GetRepoData.swift
@@ -10,16 +10,16 @@ import Alamofire
 class GetRepoData{
     static let shared = GetRepoData()
     func getRepoData(completion: @escaping (Result<[Repository], Error>) -> Void) {
-            let url = "https://api.github.com/users/devpark435/repos"
-            
-            AF.request(url).responseDecodable(of: [Repository].self) { response in
-                switch response.result {
-                case .success(let repository):
-                    completion(.success(repository))
-                case .failure(let error):
-                    print("Error: \(error)")
-                    completion(.failure(error))
-                }
+        let url = "https://api.github.com/users/devpark435/repos"
+        
+        AF.request(url).responseDecodable(of: [Repository].self) { response in
+            switch response.result {
+            case .success(let repository):
+                completion(.success(repository))
+            case .failure(let error):
+                print("Error: \(error)")
+                completion(.failure(error))
             }
         }
+    }
 }
--- GithubProfile/Network/GetUserData.swift
+++ GithubProfile/Network/GetUserData.swift
@@ -10,17 +10,17 @@ import Alamofire
 class GetUserData{
     static let shared = GetUserData()
     func getUserData(completion: @escaping (Result<User, Error>) -> Void) {
-            let url = "https://api.github.com/users/devpark435"
-            
-            AF.request(url).responseDecodable(of: User.self) { response in
-                switch response.result {
-                case .success(let user):
-                    User.shared.update(with: user)
-                    completion(.success(user))
-                case .failure(let error):
-                    print("Error: \(error)")
-                    completion(.failure(error))
-                }
+        let url = "https://api.github.com/users/devpark435"
+        
+        AF.request(url).responseDecodable(of: User.self) { response in
+            switch response.result {
+            case .success(let user):
+                User.shared.update(with: user)
+                completion(.success(user))
+            case .failure(let error):
+                print("Error: \(error)")
+                completion(.failure(error))
             }
         }
+    }
 }
