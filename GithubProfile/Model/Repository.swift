2cc7bb9eb4d77f4d1e24795f5f75c6cf0b5f5ece
devpark435 <devpark435@gmail.com>
2024-04-10 01:49:56 +0900

    feat #4 - RepoList Data Model

--- GithubProfile/Model/Repository.swift
+++ GithubProfile/Model/Repository.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+struct Repository: Codable {
+    let id: Int
+    let name: String
+    let fullName: String
+    let owner: Owner
+    let description: String?
+    let language: String?
+    let htmlURL: String
+    // 필요한 다른 프로퍼티 추가
+    
+    enum CodingKeys: String, CodingKey {
+        case id, name
+        case fullName = "full_name"
+        case owner
+        case description
+        case language
+        case htmlURL = "html_url"
+    }
+}
+
+struct Owner: Codable {
+    let login: String
+    let avatarURL: String
+    
+    enum CodingKeys: String, CodingKey {
+        case login
+        case avatarURL = "avatar_url"
+    }
+}
