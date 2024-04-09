eb6d122cb8e92c14fa86359c64d79459857b993f
devpark435 <devpark435@gmail.com>
2024-04-10 01:49:41 +0900

    feat #4 - RepoList Data Model

--- GithubProfile/Network/GetRepoData.swift
+++ GithubProfile/Network/GetRepoData.swift
@@ -5,4 +5,21 @@
 //  Created by 박현렬 on 4/10/24.
 //
 
-import Foundation
+import Alamofire
+
+class GetRepoData{
+    static let shared = GetRepoData()
+    func getRepoData(completion: @escaping (Result<[Repository], Error>) -> Void) {
+            let url = "https://api.github.com/users/devpark435/repos"
+            
+            AF.request(url).responseDecodable(of: [Repository].self) { response in
+                switch response.result {
+                case .success(let repository):
+                    completion(.success(repository))
+                case .failure(let error):
+                    print("Error: \(error)")
+                    completion(.failure(error))
+                }
+            }
+        }
+}
