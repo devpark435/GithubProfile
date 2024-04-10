66290008228b371b7868ec215b181f1bad301f85
devpark435 <devpark435@gmail.com>
2024-04-10 22:37:16 +0900

    feat #4 - Connect Tablevie & OrganizationTableCell

--- GithubProfile/Presentation/Organization/ViewController/OrganizationViewController.swift
+++ GithubProfile/Presentation/Organization/ViewController/OrganizationViewController.swift
@@ -5,4 +5,52 @@
 //  Created by 박현렬 on 4/10/24.
 //
 
-import Foundation
+import UIKit
+import Then
+
+class OrganizationViewController: UIViewController{
+    
+    @IBOutlet weak var organizationTableView: UITableView!
+    var orgs: [OrganizationModel] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        GetOrganizationData.shared.getOrganizationData { result in
+            switch result {
+            case .success(let organizations):
+                self.orgs = organizations
+                
+                DispatchQueue.main.async {
+                    self.organizationTableView.reloadData()
+                }
+            case .failure(let error):
+                // 에러 처리
+                print("Error: \(error)")
+            }
+        }
+        organizationTableView.delegate = self
+        organizationTableView.dataSource = self
+    }
+}
+extension OrganizationViewController: UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return orgs.count
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "OrganizationTableCell", for: indexPath) as! OrganizationTableCell
+        
+        let org = orgs[indexPath.row]
+        cell.configure(org: org)
+        
+        return cell
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let org = orgs[indexPath.row]
+        
+        guard let url = URL(string: org.url) else { return }
+        UIApplication.shared.open(url, options: [:], completionHandler: nil)
+    }
+}
